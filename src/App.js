36a98b04c325e31d68522b36ee8032e49e225127
Andriidubyk <andrii.dublik@gmail.com>
2023-12-11 16:33:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [items, setItems] = useState([]);
+  function onHandleItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function onHandleDelete(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function onHandleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="title">Shopping items</div>
+      <ItemInput items={items} onHandleItems={onHandleItems} />
+      <Items
+        items={items}
+        onHandleDelete={onHandleDelete}
+        onHandleToggleItem={onHandleToggleItem}
+      />
     </div>
   );
 }
 
-export default App;
+function ItemInput({ items, onHandleItems }) {
+  const [value, setValue] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!value) return;
+    const id = crypto.randomUUID();
+
+    const newItem = { value, id, packed: false };
+    onHandleItems(newItem);
+    setValue("");
+  }
+
+  return (
+    <form className="input-container" onSubmit={handleSubmit}>
+      <div className="item-input">
+        <input
+          type="text"
+          value={value}
+          maxLength="26"
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Add an item"
+        />
+      </div>
+      <div className="add-button" onClick={handleSubmit}>
+        Add
+      </div>
+    </form>
+  );
+}
+
+function Items({ items, onHandleDelete, onHandleToggleItem }) {
+  return (
+    <ul className="item-list">
+      {items.map((val) => (
+        <div className="item" key={val.value}>
+          <span style={val.packed ? { textDecoration: "line-through" } : {}}>
+            {val.value}
+          </span>
+
+          <div className="actions">
+            <input
+              type="checkbox"
+              onChange={() => onHandleToggleItem(val.id)}
+            />
+            <div
+              className="delete-button"
+              onClick={() => onHandleDelete(val.id)}
+            >
+              Delete
+            </div>
+          </div>
+        </div>
+      ))}
+    </ul>
+  );
+}
